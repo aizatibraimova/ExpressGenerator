8ca871dda4cc24a18b3274684185dd38ad664672
aizatibraimova <97326728+aizatibraimova@users.noreply.github.com>
2023-08-19 11:33:43 -0500

    Week 2, Task 2

---
+++ models/promotion.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+require("mongoose-currency").loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const promotionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    cost: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Promotion = mongoose.modell("Promotion", promotionSchema);
+
+module.exports = Promotion;
