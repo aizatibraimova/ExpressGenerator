663fdb370464201e1827f24084006c8693d94db7
aizatibraimova <97326728+aizatibraimova@users.noreply.github.com>
2023-08-27 20:10:10 -0500

    Week 3 Assignments

--- authenticate.js
+++ authenticate.js
@@ -32,6 +32,16 @@ exports.jwtPassport = passport.use(
 
 exports.verifyUser = passport.authenticate("jwt", { session: false });
 
+exports.verifyAdmin = function (req, res, next) {
+  if (req.user.admin) {
+    return next();
+  } else {
+    const err = new Error("You are not authorized to perform this operation!");
+    err.status = 403;
+    return next(err);
+  }
+};
+
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
